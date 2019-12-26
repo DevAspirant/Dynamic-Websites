f5cb1cc0e05826116e2e3896b0fa430241de7aed
Falmban Ammar <devAspirant@gmail.com>
2019-12-26 16:56:58 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link href="/assets/style.css" rel='stylesheet'>
+</head>
+<body id='final-example'>
+	<div class='wrapper'>
+		<div class='banner'></div><!-- banner -->
+		<div id='nav'></div><!-- navigation -->
+		<div class='content'>
+		<div id='footer' class='cf'>
+			<div class='column three'>
+				<strong>Phone</strong> 808.529.2919
+			</div><!-- column -->
+			<div class='column three'>
+			<strong>Location</strong>
+			123 mecca<br>
+			</div><!-- column -->
+			<div class='column three last'></div><!-- column -->
+			<strong> Hours </strong>
+			<em> Tusday - Thursday </em><br>
+			1:00 pm - 9:00 pm<br><br>
+			<em> Friday - Saturday </em><br>
+			4:00 pm - 11:00 pm<br><br>
+			<em> Susday - Monday </em><br>
+			Closed <br><br>
+		</div><!-- footer -->
+		</div><!-- content -->
+		<small>&copy;2014 Ammar</small>
+	</div><!-- wrapper -->
+	<div class='copyright-info'>
+		<?php include('..//assets/includes/copyright.php'); ?>
+	</div><!-- copyright-info -->
+</body>
+</html>
